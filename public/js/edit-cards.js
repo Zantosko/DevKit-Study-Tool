ed5836a1884bb10e4726ef387368c39ebada3453
Zach Antosko <zachary.antosko@gmail.com>
2021-05-10 19:02:54 -0500

    modified edit-cards page

--- public/js/edit-cards.js
+++ public/js/edit-cards.js
@@ -1,11 +1,19 @@
 const flashCard = document.querySelector(".flashcard");
 const editCardBtn = document.querySelector(".edit-btn");
+const exitBtn = document.querySelector(".exit-btn");
 
 const flash = (e) => {
-  console.log("click")
   flashCard.style.transform = "rotateX(180deg)"
 
   e.preventDefault()
 }
 
+const flashBack = (e) => {
+  flashCard.style.transform = "rotateX(0deg)"
+
+  e.preventDefault()
+}
+
 editCardBtn.addEventListener("click", flash);
+
+exitBtn.addEventListener("click", flashBack);
